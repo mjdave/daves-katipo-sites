58a497457fc3508553b4c297c816f291b2e10e91
Dave Frampton <dave@afk.nz>
2026-05-04 03:40:07 +0000

    minor tweaks

--- startTracker.sh
+++ startTracker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if cd katipo/apps/katipoTracker && ./build.sh; then
-cd ../../.. && ./katipo/apps/katipoTracker/katipoTracker
+cd ../../.. && ./katipo/apps/katipoTracker/katipoTracker --defaultHost katipo
 fi
