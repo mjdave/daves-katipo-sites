a026a7489191ab9300553e0f61f6573f93ac2425
David Frampton <davidframpton@majicjungle.com>
2026-08-03 07:13:50 +1200

    add solitaire, not playable yet

---
+++ solitaire/start.bat
@@ -0,0 +1,4 @@
+start C:\Users\kids\Documents\katipoBrowser\windows\x64\Release\katipoBrowser.exe
+start code C:\Users\kids\Documents\solitaire
+start C:\Users\kids\Documents\katipoBrowser\katipo\apps\katipoTracker\x64\Debug\katipoTracker.exe
+start C:\Users\kids\Documents\katipoBrowser\katipo\apps\katipoHost\x64\Debug\katipoHost.exe --site C:\Users\kids\Documents\solitaire
